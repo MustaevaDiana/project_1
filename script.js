69a3b74638ea1f1bc1c82023c25d8bc5237ced7f
Diana <mystaeva.diana99@gmail.com>
2022-02-28 06:43:28 +0500

    HW2

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 " use strict";
 
 const numberofFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+        count:numberofFilms,
+        movies:{},
+        actors:{},
+        genres:[],
+        privat:false,
+};
+
